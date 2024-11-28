c238ff1284391260942a56fcb678dc2bd4f72004
lsb <likith15600@gmail.com>
2024-11-28 20:24:37 +0530

    Added production fronted url to backend

--- Backend/index.js
+++ Backend/index.js
@@ -6,7 +6,10 @@ const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://campus-connect-fronted.vercel.app/",
+    ],
   },
 });
 
