2f1c958131bb38cb7730b8f10072a0e27a284409
lsb <likith15600@gmail.com>
2024-11-28 20:13:15 +0530

    Added env variables to access the backend url

--- Frontend/Campus_connect/src/services/Socket.ts
+++ Frontend/Campus_connect/src/services/Socket.ts
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
 
-const url = "http://localhost:3000";
+// const url = "http://localhost:3000";
+const url = import.meta.env.VITE_SOCKET_URL;
 
 const socket = io(url, { autoConnect: false, reconnection: true }); // will connect the user after he enters his username
 
