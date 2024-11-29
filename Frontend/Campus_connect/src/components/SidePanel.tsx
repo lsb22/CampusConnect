5cbd3db3628750a9bca483f8b779c84c249363f8
lsb <likith15600@gmail.com>
2024-11-29 19:01:32 +0530

    Added green dot to active users

--- Frontend/Campus_connect/src/components/SidePanel.tsx
+++ Frontend/Campus_connect/src/components/SidePanel.tsx
@@ -1,4 +1,4 @@
-import { VStack, Text, Box } from "@chakra-ui/react";
+import { VStack, Text, Box, HStack } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import socket from "../services/Socket";
 
@@ -29,7 +29,10 @@ const SidePanel = () => {
         <Box fontSize="1.4rem">
           {users.map((user, idx) => (
             <Box key={idx} mb={3}>
-              {user.username}
+              <HStack>
+                <Box className="active-users-dot" mr={3} />
+                <Text fontSize="1.6rem">{user.username}</Text>
+              </HStack>
             </Box>
           ))}
         </Box>
