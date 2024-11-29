796e5d7549e8694cf117d65ccf55555fa949a212
lsb <likith15600@gmail.com>
2024-11-29 18:22:10 +0530

    configured socket in frontend

--- Frontend/Campus_connect/src/services/Socket.ts
+++ Frontend/Campus_connect/src/services/Socket.ts
@@ -1,8 +1,12 @@
 import { io } from "socket.io-client";
 
-// const url = "http://localhost:3000";
 const url = import.meta.env.VITE_SOCKET_URL;
 
-const socket = io(url, { autoConnect: false, reconnection: true }); // will connect the user after he enters his username
+const socket = io(url, {
+  autoConnect: false,
+  reconnection: true,
+  withCredentials: true,
+  transports: ["websocket", "polling"],
+}); // will connect the user after he enters his username
 
 export default socket;
