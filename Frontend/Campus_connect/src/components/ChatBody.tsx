a3913cfcec13da5feeedfe420623896b992756d7
lsb <likith15600@gmail.com>
2024-12-20 09:29:17 +0530

    changed arrow down button position

--- Frontend/Campus_connect/src/components/ChatBody.tsx
+++ Frontend/Campus_connect/src/components/ChatBody.tsx
@@ -4,6 +4,7 @@ import { Socket } from "socket.io-client";
 import RenderImage from "./RenderImage";
 import { useEffect, useRef } from "react";
 import arrow from "../assets/images/down-arrows.png";
+
 interface Props {
   messages: MessageStruct[];
   socket: Socket;
@@ -85,6 +86,7 @@ const ChatBody = ({ messages, socket, show }: Props) => {
       {show && (
         <Button
           className="arrow-down"
+          variant="outline"
           position="fixed"
           bgColor="white"
           onClick={handleclick}
@@ -92,37 +94,6 @@ const ChatBody = ({ messages, socket, show }: Props) => {
           <Image className="arrow-img" src={arrow} />
         </Button>
       )}
-
-      {/* {messages.map((message, idx) => (
-        <Flex
-          flex="1fr"
-          justifyContent={
-            message.userName === socket.username ? "end" : "start"
-          }
-          key={idx}
-          mt={10}
-        >
-          <VStack
-            alignItems={message.userName === socket.username ? "end" : "start"}
-          >
-            <Text p={1} className="username">
-              {message.userName === socket.username ? "You" : message.userName}
-            </Text>
-            <Text
-              className="message-text"
-              bgColor={
-                message.userName === socket.username ? "teal" : "lightcoral"
-              }
-              px={10}
-              py={2}
-              borderRadius={10}
-              maxWidth="600px"
-            >
-              {message.text}
-            </Text>
-          </VStack>
-        </Flex>
-      ))} */}
     </Box>
   );
 };
